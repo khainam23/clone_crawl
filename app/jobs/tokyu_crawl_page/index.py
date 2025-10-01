e09b1a27f81e7e44a26cbee0431a25a8c49b78fc
KhaiNam <khainam23@gmail.com>
2025-10-01 16:07:48 +0700

    new

--- app/jobs/tokyu_crawl_page/index.py
+++ app/jobs/tokyu_crawl_page/index.py
@@ -6,27 +6,24 @@ from bs4 import BeautifulSoup
 
 from app.jobs.crawl_strcture.index import crawl_pages
 from .custom_extractor_factory import setup_custom_extractor
-from . import constants as config
+from .constants import URL_MULTI, ITEM_SELECTOR, NUM_PAGES, BASE_URL
+from app.core.config import settings
 
 async def crawl_multi():
-    url = config.url_multi
-    item_selector = config.item_selector
-    num_page = 30
-
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        "User-Agent": settings.CRAWLER_USER_AGENT
     }
     urls = []
 
-    for page in range(1, num_page + 1):
-        page_url = f"{url}{page}"
+    for page in range(1, NUM_PAGES + 1):
+        page_url = f"{URL_MULTI}{page}"
         resp = requests.get(page_url, headers=headers)
         if resp.status_code != 200:
             print(f"❌ Lỗi tải trang {page}")
             continue
         
         soup = BeautifulSoup(resp.text, "html.parser")
-        items = soup.select(item_selector)
+        items = soup.select(ITEM_SELECTOR)
         
         print(f"Trang {page}: tìm thấy {len(items)} items")
 
@@ -38,14 +35,14 @@ async def crawl_multi():
                 if href.startswith('http'):
                     full_url = href
                 else:
-                    full_url = config.BASE_URL + href
+                    full_url =  BASE_URL + href
                 urls.append(full_url)
             
         
     await crawl_pages(
         urls, 
-        batch_size=config.batch_size, 
-        id_mongo=config.ID_MONGO, 
-        collection_name=config.COLLECTION_NAME,
+        batch_size= settings.BATCH_SIZE, 
+        id_mongo= settings.ID_MONGO_TOKYU, 
+        collection_name= settings.COLLECTION_NAME_TOKYU,
         custom_extractor_factory=setup_custom_extractor  # Pass the factory function
     )
