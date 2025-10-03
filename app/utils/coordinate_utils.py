811ef1b3fad0960746a815b7eaef79b2fb4d7867
khainam23 <khainam23@gmail.com>
2025-10-03 16:56:46 +0700

    new

--- app/utils/coordinate_utils.py
+++ app/utils/coordinate_utils.py
@@ -14,7 +14,7 @@ def fetch_coordinates_from_google_maps(address: str) -> Optional[Tuple[float, fl
         google_maps_url = f"https://www.google.co.jp/maps/place/{encoded_address}"
 
         chrome_options = Options()
-        chrome_options.binary_location = CHROME_BIN  # Bắt buộc trên Kaggle
+        chrome_options.binary_location = "/usr/bin/chromium-browser"  # Bắt buộc trên Kaggle
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
@@ -24,7 +24,7 @@ def fetch_coordinates_from_google_maps(address: str) -> Optional[Tuple[float, fl
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36")
 
         # Chromedriver đã cài sẵn trên Kaggle
-        service = Service(CHROMEDRIVER_PATH)
+        service = Service("/usr/bin/chromedriver")
         driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.set_page_load_timeout(15)
         driver.get(google_maps_url)
